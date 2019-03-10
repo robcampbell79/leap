38f80b57be151bb2e98020ca4403a4bcba7695f3
Higalgo <rhc91310@gmail.com>
2019-03-10 18:03:05 -0500

    Add logic for determining leap year

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+
+    let mut is_leap = false;
+
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                is_leap = true;
+            } else {
+                is_leap = false;
+            }
+        } else {
+            is_leap = true;
+        }
+    } else {
+        is_leap = false;
+    }
+
+    is_leap
 }
